c1c0c2a5e83c46c8c40ace2a633ec277c44a519c
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-01-21 12:53:10 +0530

    basic

---
+++ first.js
@@ -0,0 +1,14 @@
+import {evenOdd,add} from './modules.js'
+
+
+var a = 21;
+var b = 23;
+var c = 22;
+var d = 33;
+
+evenOdd(a)
+evenOdd(b)
+evenOdd(c)
+evenOdd(d)
+add(2,3)
+sub(4,2)
---
+++ modules.js
@@ -0,0 +1,20 @@
+export function evenOdd(x) {
+  if (x % 2 == 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
+
+export function add(a,b){
+    console.log(a+b);
+}
+function sub(a,b){
+    console.log(a-b);
+}
+function mul(a,b){
+    console.log(a*b);
+}
+function div(a,b){
+    console.log(a/b);
+}
